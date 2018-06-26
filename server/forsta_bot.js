504159d249569cd9257bc1836827a8d293ea3bbb
Zach Richardson <zach@tyrocoders.com>
2018-06-26 16:39:56 -0600

    added additional necessary functions to forsta_bot

--- server/forsta_bot.js
+++ server/forsta_bot.js
@@ -3,7 +3,15 @@
 const BotAtlasClient = require('./atlas_client');
 const cache = require('./cache');
 const relay = require('librelay');
+const uuid4 = require("uuid/v4");
+const moment = require("moment");
+const words = require("./authwords");
 
+const AUTH_FAIL_THRESHOLD = 10;
+
+async function sleep(ms) { 
+    return await new Promise(resolve => setTimeout(resolve, ms)); 
+}
 
 class ForstaBot {
 
@@ -49,6 +57,10 @@ class ForstaBot {
         console.error('Message Error', e, e.stack);
     }
 
+    fqTag(user) { return `@${user.tag.slug}:${user.org.slug}`; }
+    fqName(user) { return [user.first_name, user.middle_name, user.last_name].map(s => (s || '').trim()).filter(s => !!s).join(' '); }
+    fqLabel(user) { return `${this.fqTag(user)} (${this.fqName(user)})`; }
+
     async onMessage(ev) {
         const message = ev.data.message;
         const msgEnvelope = JSON.parse(message.body);
@@ -75,6 +87,198 @@ class ForstaBot {
             html: `${ reply }`,
             text: reply
         });
+    }
+
+    forgetStaleNotificationThreads() {
+        let tooOld = new Date();
+        tooOld.setDate(tooOld.getDate() - 7);
+
+        Object.keys(this.notificationThread).forEach(n => {
+            if (this.notificationThread[n].flaggedEntry.received < tooOld) {
+                delete this.notificationThread[n];
+            }
+        });
+        console.log('stale notification threads removed. currently tracking:', Object.assign({}, this.notificationThread));
+    }
+
+    async incrementAuthFailCount() {
+        let fails = await relay.storage.get('authentication', 'fails', {count: 0, since: new Date()});
+        fails.count++;
+
+        if (fails.count >= AUTH_FAIL_THRESHOLD) {
+            await this.broadcastNotice({
+                note: `SECURITY ALERT!\n\n${fails.count} failed login attempts (last successful login was ${moment(fails.since).fromNow()})`
+            });
+        }
+
+        await relay.storage.set('authentication', 'fails', fails);
+    }
+
+    async resetAuthFailCount() {
+        await relay.storage.set('authentication', 'fails', {count: 0, since: new Date()});
+    }
+
+    async getSoloAuthThreadId() {
+        let id = await relay.storage.get('authentication', 'soloThreadId');
+        if (!id) {
+            id = uuid4();
+            relay.storage.set('authentication', 'soloThreadId', id);
+        }
+
+        return id;
+    }
+
+    async getGroupAuthThreadId() {
+        let id = await relay.storage.get('authentication', 'groupThreadId');
+        if (!id) {
+            id = uuid4();
+            relay.storage.set('authentication', 'groupThreadId', id);
+        }
+
+        return id;
+    }
+
+    genAuthCode(expirationMinutes) {
+        const code = `${words.adjective()} ${words.noun()}`;
+        const expires = new Date();
+        expires.setMinutes(expires.getMinutes() + expirationMinutes);
+        return { code, expires };
+    }
+
+    removeExpiredAuthCodes(pending) {
+        const now = new Date();
+
+        Object.keys(pending).forEach(uid => {
+            pending[uid].expires = new Date(pending[uid].expires);
+            if (pending[uid].expires < now) {
+                delete pending[uid];
+            }
+        });
+
+        return pending;
+    }
+
+    async sendAuthCode(tag) {
+        tag = (tag && tag[0] === '@') ? tag : '@' + tag;
+        const resolved = await this.resolveTags(tag);
+        if (resolved.userids.length === 1 && resolved.warnings.length === 0) {
+            const uid = resolved.userids[0];
+            const adminIds = await relay.storage.get('authentication', 'adminIds');
+            if (!adminIds.includes(uid)) {
+                throw { statusCode: 403, info: { tag: ['not an authorized user'] } }; 
+            }
+
+            const auth = this.genAuthCode(1);
+            console.log(auth, resolved);
+            this.msgSender.send({
+                distribution: resolved,
+                threadTitle: 'Message Bot Login',
+                threadId: await this.getGroupAuthThreadId(),
+                text: `codewords: ${auth.code}\n(valid for one minute)`
+            });
+            const pending = await relay.storage.get('authentication', 'pending', {});
+            pending[uid] = auth;
+            await relay.storage.set('authentication', 'pending', pending);
+            
+            return resolved.userids[0];
+        } else {
+            throw { statusCode: 400, info: { tag: ['not a recognized tag, please try again'] } }; 
+        }
+    }
+
+    async validateAuthCode(userId, code) {
+        console.log(userId, code);
+        let pending = await relay.storage.get('authentication', 'pending', {});
+        pending = this.removeExpiredAuthCodes(pending);
+        const auth = pending[userId];
+        if (!auth) {
+            throw { statusCode: 403, info: { code: ['no authentication pending, please start over'] } }; 
+        }
+        if (auth.code != code) {
+            this.incrementAuthFailCount();
+            await sleep(500); // throttle guessers
+            throw { statusCode: 403, info: { code: ['incorrect codewords, please try again'] } }; 
+        }
+
+        delete pending[userId];
+        relay.storage.set('authentication', 'pending', pending);
+
+        await this.broadcastNotice({note: 'LOGIN', actorUserId: userId, listAll: false});
+        await this.resetAuthFailCount();
+        return true;
+    }
+
+    async getAdministrators() {
+        const adminIds = await relay.storage.get('authentication', 'adminIds', []);
+        const adminUsers = await this.getUsers(adminIds);
+        const admins = adminUsers.map(u => {
+            return {
+                id: u.id,
+                label: this.fqLabel(u)
+            };
+        });
+        return admins;
+    }
+
+    async broadcastNotice({note, actorUserId, listAll=true}) {
+        const adminIds = await relay.storage.get('authentication', 'adminIds', []);
+        let added = false;
+        if (actorUserId && !adminIds.includes(actorUserId)) {
+            adminIds.push(actorUserId);
+            added = true;
+        }
+        const adminUsers = await this.getUsers(adminIds);
+        const actor = adminUsers.find(u => u.id === actorUserId);
+        const actorLabel = actor ? this.fqLabel(actor) : '<unknown>';
+        const expression = adminUsers.map(u => this.fqTag(u)).join(' + ');
+        const distribution = await this.resolveTags(expression);
+
+        const adminList = adminUsers.filter(u => !(added && u.id === actorUserId)).map(u => this.fqLabel(u)).join('\n');
+
+        let fullMessage = note;
+        fullMessage += actorUserId ? `\n\nPerformed by ${actorLabel}` : '';
+        fullMessage += listAll ? `\n\nCurrent authorized users:\n${adminList}` : '';
+        fullMessage = fullMessage.replace(/<<([^>]*)>>/g, (_, id) => {
+            const user = adminUsers.find(x => x.id === id);
+            return this.fqLabel(user);
+        });
+
+        this.msgSender.send({
+            distribution,
+            threadTitle: 'Compliance Alerts',
+            threadId: await this.getSoloAuthThreadId(),
+            text: fullMessage
+        });
+    }
+
+    async addAdministrator({addTag, actorUserId}) {
+        const tag = (addTag && addTag[0] === '@') ? addTag : '@' + addTag;
+        const resolved = await this.resolveTags(tag);
+        if (resolved.userids.length === 1 && resolved.warnings.length === 0) {
+            const uid = resolved.userids[0];
+            const adminIds = await relay.storage.get('authentication', 'adminIds');
+            if (!adminIds.includes(uid)) {
+                adminIds.push(uid);
+                await relay.storage.set('authentication', 'adminIds', adminIds);
+            }
+            await this.broadcastNotice({note: `ADDED <<${uid}>> to authorized users`, actorUserId});
+            return this.getAdministrators();
+        }
+        throw { statusCode: 400, info: { tag: ['not a recognized tag, please try again'] } }; 
+    }
+
+    async removeAdministrator({removeId, actorUserId}) {
+        const adminIds = await relay.storage.get('authentication', 'adminIds', []);
+        const idx = adminIds.indexOf(removeId);
+
+        if (idx < 0) {
+            throw { statusCode: 400, info: { id: ['administrator id not found'] } };
+        }
+        adminIds.splice(idx, 1);
+        await this.broadcastNotice({note: `REMOVING <<${removeId}>> from authorized users`, actorUserId});
+        await relay.storage.set('authentication', 'adminIds', adminIds);
+
+        return this.getAdministrators();
     }
 }
 
