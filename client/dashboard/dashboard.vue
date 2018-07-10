cc12f2c7bda21e94c64c1953f1d4993e2a9f8e41
mcavoybn <mcavoybn@gmail.com>
2018-07-10 11:45:26 -0600

    Edit business hours form and some scaffolding

--- client/dashboard/dashboard.vue
+++ client/dashboard/dashboard.vue
@@ -1,14 +1,107 @@
 <style>
+.checker-outline-hover:hover{
+    border-style: dashed;
+    border-width: 1px;
+    padding:5px;
+}
+input{
+    padding:2px;
+}
 </style>
 
-<template>
+<template lang="html">
 <div>
     <div class="ui container center aligned">
-        <div class="ui basic segment huge">
-            <h1 class="ui header">
-                <i class="large circular asterisk loading icon"></i>
-                Messaging Bot Running
-            </h1>
+        
+
+        <sui-accordion exclusive>
+            <sui-accordion-title>
+                <sui-icon name="dropdown" />
+                <span>Edit Business Hours</span>
+            </sui-accordion-title>
+            <sui-accordion-content> 
+                <sui-grid>
+                    <sui-grid-row>
+                        <sui-grid-column :width="6">
+                            <div class="ui labeled input">
+                                <div class="ui label">Open:</div>
+                                <input format="HH:MM:SS" v-model="openTime" type="time"/>
+                            </div>
+                            <div class="ui labeled input">
+                                <div class="ui label">Close:</div>
+                                <input format="HH:MM:SS" v-model="closeTime" type="time"/>
+                            </div>
+                        </sui-grid-column>
+                        <sui-grid-column :width="10">
+                            <div class="ui form">
+                                <div class="field">
+                                    <textarea placeholder="Out of office message" 
+                                    rows="4"></textarea>
+                                </div>
+                            </div>
+                        </sui-grid-column>
+                    </sui-grid-row>
+                    <sui-grid-row>
+                        <sui-grid-column :width="18">
+                            
+                        </sui-grid-column>
+                    </sui-grid-row>                    
+                </sui-grid>               
+            </sui-accordion-content>
+        </sui-accordion>
+
+        <sui-accordion exclusive v-for="question in questions">
+
+            <sui-accordion-title>
+                <sui-icon name="dropdown" />
+                <span 
+                    v-if="!question.editingTitle"
+                    @dblclick="editTitle(question)"
+                    v-text="question.title"
+                    class="checker-outline-hover"></span>
+
+                <sui-input 
+                    v-model="question.title"
+                    :value="question.title" 
+                    v-if="question.editingTitle"></sui-input>
+                    
+                <sui-button 
+                    icon="save outline" 
+                    v-if="question.editingTitle"
+                    @click="editTitle(question)"
+                    primary></sui-button>
+            </sui-accordion-title>
+
+            <sui-accordion-content>
+                <sui-grid>
+                    <sui-grid-row>
+                        <sui-grid-column :width="10">
+                            <!-- response names dropdowns -->
+                        </sui-grid-column>
+
+                        <sui-grid-column :width="6">
+                            <!-- onselect dropdowns -->
+                        </sui-grid-column>
+                    </sui-grid-row>
+                    <sui-grid-row>
+                        <sui-grid-column :width="12">
+                            <!-- new question response button -->
+                        </sui-grid-column>
+                        
+                        <sui-grid-column :width="4">
+                            <!-- question type dropdown -->
+                        </sui-grid-column>
+                    </sui-grid-row>
+                </sui-grid>                
+            </sui-accordion-content>
+
+        </sui-accordion>
+
+        <button class="ui button"
+            @click="newQuestion()">New Question</button>
+        
+        <div class="ui basic segment">
+            <button @click="save()" class="ui button">Save Changes</button>
         </div>
     </div>
 </div>
@@ -17,9 +110,43 @@
 <script>
 module.exports = {
     data: () => ({ 
-        global: shared.state
+        global: shared.state,
+        questions: [
+            {
+                id: 1,
+                title:"Question 1",
+                responses: [
+                    "Response 1",
+                    "Response 2",
+                    "Response 3"
+                ],
+                editingTitle: false
+            },
+            {
+                id: 2,
+                title:"Question 2",
+                responses: [
+                    "Response 1",
+                    "Response 2",
+                    "Response 3"
+                ],
+                editingTitle: false
+            }
+        ]
     }),
     methods: {
+        save: function() {
+            //save all the form data to the API
+        },
+        newQuestion: function () {
+            //creates a new question for the currently loaded question set
+        },
+        newResponse: function (question){
+            //add a blank new response based on the question type
+        },
+        editTitle: function (question) {
+            question.editingTitle = !question.editingTitle;
+        }
     },
     mounted: function() {
         console.log(this.global.onboardStatus, this.$router.path);
@@ -38,6 +165,40 @@ module.exports = {
         if (!this.global.apiToken) {
             this.$router.push({ name: 'loginTag', query: { forwardTo: this.$router.path }});
             return;
+        }
+    },
+    computed: {
+        openTime: {
+            get: function() {
+                return "08:00:00";
+            },
+            set: function() {
+
+            }
+        },
+        closeTime: {
+            get: function() {
+                return "08:00:00";
+            },
+            set: function() {
+
+            }
+        },
+        messageBotConfig: {
+            get: function() {
+
+            },
+            set: function() {
+
+            }
+        },
+        OooMessage: {
+            get: function() {
+
+            },
+            set: function() {
+
+            }
         }
     }
 }
